14cf63e6e02f169f9c74344014ce5c6cf908e5af
GMkonan <guilhemont@hotmail.com>
2023-12-04 14:57:02 -0300

    Day two

--- src/2/a.ts
+++ src/2/a.ts
@@ -1,7 +1,56 @@
 const input = await Bun.file(`${import.meta.dir}/input.txt`).text();
+// x number of cubes in bag
+// either red green or blue
 
-// Some really crazy and complex code later...
+// possible games with:
+// 12 red cubes, 13 green cubes and 14 blue cubes
 
-const result = input;
+const regexNumber = /\d+/g;
+const regexText = /[a-zA-Z]+/g;
 
+let games: string[] = [];
+
+const result = input.split("\n").map((game) => {
+  const gameId = game.split(":")[0];
+
+  const cubeSets = game.split(":")[1].split(";");
+  let impossibleGame = false;
+  let possibleGameId = "";
+  for (let set of cubeSets) {
+    let impossible = false;
+    const cubesByColor = set.split(",");
+    for (let cube of cubesByColor) {
+      const number = cube.match(regexNumber);
+      const color = cube.match(regexText);
+      // imposible games
+      if (color![0] === "red" && (number![0] as unknown as number) > 12) {
+        impossible = true;
+        break;
+      }
+      if (color![0] === "green" && (number![0] as unknown as number) > 13) {
+        impossible = true;
+        break;
+      }
+      if (color![0] === "blue" && (number![0] as unknown as number) > 14) {
+        impossible = true;
+        break;
+      }
+      possibleGameId = gameId;
+    }
+    if (impossible === true) {
+      impossibleGame = true;
+      break;
+    }
+  }
+  if (!impossibleGame) {
+    games.push(possibleGameId);
+  }
+});
+
+const final = games
+  .map((game) => game.match(regexNumber)![0])
+  .map(Number)
+  .reduce((a, b) => a + b);
+
+console.log({ final });
 console.log({ result });
--- src/2/b.ts
+++ src/2/b.ts
@@ -1,7 +1,48 @@
 const input = await Bun.file(`${import.meta.dir}/input.txt`).text();
+// x number of cubes in bag
+// either red green or blue
 
-// Some really crazy and complex code later...
+// possible games with:
+// 12 red cubes, 13 green cubes and 14 blue cubes
 
-const result = input;
+const regexNumber = /\d+/g;
+const regexText = /[a-zA-Z]+/g;
 
+let games: string[] = [];
+
+const result = input.split("\n").map((game) => {
+  const gameId = game.split(":")[0];
+  const cubeSets = game.split(":")[1].split(";");
+  let redCubes = 0;
+  let blueCubes = 0;
+  let greenCubes = 0;
+  for (let set of cubeSets) {
+    const cubesByColor = set.split(",");
+    for (let cube of cubesByColor) {
+      const number = cube.match(regexNumber)?.map(Number);
+      const color = cube.match(regexText);
+
+      if (color![0] === "red" && (redCubes < number![0] || redCubes == 0)) {
+        console.log(color![0]);
+        console.log({ redCubes });
+        redCubes = number![0];
+      }
+      if (
+        color![0] === "green" &&
+        (greenCubes < number![0] || greenCubes == 0)
+      ) {
+        greenCubes = number![0];
+      }
+      if (color![0] === "blue" && (blueCubes < number![0] || blueCubes == 0)) {
+        blueCubes = number![0];
+      }
+    }
+  }
+  console.log({ redCubes, greenCubes, blueCubes });
+  return redCubes * blueCubes * greenCubes;
+});
+
+const final = result.reduce((a, b) => a + b);
+
+console.log({ final });
 console.log({ result });
