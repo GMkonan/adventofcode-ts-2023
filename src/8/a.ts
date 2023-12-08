ea22b06c6c2142e771b6929ae1f815c6ac2b6e4e
GMkonan <guilhemont@hotmail.com>
2023-12-08 16:43:12 -0300

    :wrench: 8

--- src/8/a.ts
+++ src/8/a.ts
@@ -1,7 +1,25 @@
 const input = await Bun.file(`${import.meta.dir}/input.txt`).text();
 
-// Some really crazy and complex code later...
+const [directions, network] = input.split("\n\n");
+// AAA
+let currentLine = network.split("\n").find((a) => a.substring(0, 3) === "AAA")!;
+let count = 0;
 
-const result = input;
+const lookup = (direct: string) => {
+  const currentNode = currentLine.substring(0, 3)!;
+  console.log({ currentNode });
+  if (currentNode === "ZZZ") return count;
 
-console.log({ result });
+  const [leftNode, rightNode] = currentLine
+    .match(/\(([^)]+)\)/)![1]
+    .split(",")
+    .map((s) => s.trim());
+
+  direct === "L"
+    ? (currentLine = network.split("\n").find((a) => a === leftNode)!)
+    : (currentLine = network.split("\n").find((a) => a === rightNode)!);
+
+  count++;
+};
+
+directions.split("").map((direct) => lookup(direct));
