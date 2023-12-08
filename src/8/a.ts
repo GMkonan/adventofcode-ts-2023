a62cb30ca2ae2e06d55de3d066c2f50dedfe7e37
GMkonan <guilhemont@hotmail.com>
2023-12-08 18:52:39 -0300

    :wrench: 8 a end

--- src/8/a.ts
+++ src/8/a.ts
@@ -1,5 +1,7 @@
 const input = await Bun.file(`${import.meta.dir}/input.txt`).text();
 
+// should be fun to make it a tree
+
 const [directions, network] = input.split("\n\n");
 // AAA
 let currentLine = network.split("\n").find((a) => a.substring(0, 3) === "AAA")!;
@@ -7,7 +9,6 @@ let count = 0;
 
 const lookup = (direct: string) => {
   const currentNode = currentLine.substring(0, 3)!;
-  console.log({ currentNode });
   if (currentNode === "ZZZ") return count;
 
   const [leftNode, rightNode] = currentLine
@@ -16,10 +17,24 @@ const lookup = (direct: string) => {
     .map((s) => s.trim());
 
   direct === "L"
-    ? (currentLine = network.split("\n").find((a) => a === leftNode)!)
-    : (currentLine = network.split("\n").find((a) => a === rightNode)!);
+    ? (currentLine = network
+        .split("\n")
+        .find((a) => a.substring(0, 3) === leftNode)!)
+    : (currentLine = network
+        .split("\n")
+        .find((a) => a.substring(0, 3) === rightNode)!);
 
   count++;
 };
 
-directions.split("").map((direct) => lookup(direct));
+const result = () => {
+  let final = null;
+  while (!final) {
+    for (let direct of directions) {
+      final = lookup(direct);
+    }
+  }
+  console.log({ final });
+};
+
+result();
